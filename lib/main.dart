87416402a56d70f0ee7fe42e33d42eaf57a09455
Andika Putra <ara@Andikas-Mac-mini.local>
2024-06-28 01:30:41 +0800

    refactor: Remove unnecessary functions from main file

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_gutenberg_app/env/env.dart';
-
 import 'src/app.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  // ignore: prefer_const_constructors
-  runApp(MyApp());
+  runApp(const MyApp());
 }
